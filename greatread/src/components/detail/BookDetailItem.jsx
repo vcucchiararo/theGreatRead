784dc11b63538fa034805d632f3bb2aa622b5700
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-09-01 00:53:49 +0200

    create detail styles

--- greatread/src/components/detail/BookDetailItem.jsx
+++ greatread/src/components/detail/BookDetailItem.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import AccordionComponent from './AccordionComponent';
-
+// import Box from '@material-ui/core/Box';
 const BookDetailItem = ({ description, title, author, rating, image }) => {
     return (
-        <>
-            <div>
+        <div className="container-book">
+            <img
+                width="100px"
+                height="100px"
+                src="https://trello-attachments.s3.amazonaws.com/5f4d74b3457c4e749f4bfea6/847x748/f0abc621274604fc9ce77e7079853efb/GreatRead.png"
+            />
+            <div className="title">
                 <h1>{title}</h1>
                 <p>{author}</p>
-                <Link>Agregar como favorito</Link>
-                <Link>Quiero leer</Link>
-                <img src={image} />
-                <p>{rating}</p>
-                <button>Muy bueno</button>
-                <button>2810 votos</button>
-                <p>{description}</p>
             </div>
+            <div>
+                <div className="button-container">
+                    <Link>Agregar como favorito</Link>
+                    <Link>Quiero leer</Link>
+                </div>
+                <div classname="cover-book">
+                    {/* <Box boxShadow={3}> */}
+                    <img src={image} />
+                    {/* </Box> */}
+                </div>
+            </div>
+            <p className="title-rating">{rating}</p>
+            <button>Muy bueno</button>
+            <button>2810 votos</button>
+            <p>{description}</p>
             <AccordionComponent />
-        </>
+        </div>
     );
 };
 
