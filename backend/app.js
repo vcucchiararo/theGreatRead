ca21585b82fddb025334d1460a6d81cbe6dafd6d
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-08-31 11:46:15 +0200

    create server

--- backend/app.js
+++ backend/app.js
@@ -5,3 +5,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("My server works...");
+});
+
+app.listen(port, () => debug(`Running on port ${port} `));
