d91f7cdddf275d467bfe2cf7a1aee829f15eafcb
Vanesa Gómez <v.cucchiararo@gmail.com>
2020-09-03 23:44:09 +0200

    implemented slider home

--- greatread/src/components/home/bookListItem.jsx
+++ greatread/src/components/home/bookListItem.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const BookListItem = ({ id, title, cover }) => {
+const BookListItem = ({ id, title, cover, rating }) => {
     return (
-        <div>
-            <p>{title}</p>
+        <div className="container-slider">
+            <p className="book-title">{title}</p>
             <Link to={`/book/${id}`}>
-                <img src={cover} />
+                <img height="300" width="200" src={cover} />
             </Link>
+            <p className="book-title">{rating}</p>
         </div>
     );
 };
--- greatread/src/components/home/home.jsx
+++ greatread/src/components/home/home.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import './home.scss';
-// import PropTypes from 'prop-types';
+import './home.scss';
 import { loadBookList } from '../../actions/listActions';
 import listStore from '../../stores/listStore';
 import BookListItem from './BookListItem';
+import Carousel from 'react-material-ui-carousel';
+import { Paper } from '@material-ui/core';
 
 function Home(props) {
     const [bookList, setBookList] = useState(listStore.getBookList());
@@ -18,17 +19,28 @@ function Home(props) {
     }
     return (
         <>
-            {bookList &&
-                bookList.map((element) => (
-                    <BookListItem
-                        key={element.id}
-                        id={element.id}
-                        title={element.title}
-                        cover={element.image}
-                    />
-                ))}
+            <Carousel>
+                {bookList &&
+                    bookList.map((element) => (
+                        <BookListItem
+                            // key={element.id}
+                            id={element.id}
+                            title={element.title}
+                            cover={element.image}
+                            rating={element.averageRating}
+                        />
+                    ))}
+            </Carousel>
         </>
     );
+
+    function Item(props) {
+        return (
+            <Paper>
+                <h2>{props.book.name}</h2>
+            </Paper>
+        );
+    }
 }
 
 export default Home;
